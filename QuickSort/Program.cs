238817d5b0c1cb6b3833108f911c8b2a97a76614
Raihan Aria <raihan.aria.ft21@mail.umy.ac.id>
2022-10-20 16:49:22 +0700

    add array of integers to hold values

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace QuickSort
 {
-    internal class Program
+    class Program
     {
+        // array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movement
+
         static void Main(string[] args)
         {
         }
